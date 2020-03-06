81bfb101f190d82510be73416693c1f31cb44016
richo-s <richo-s@hotmail.com>
2020-03-06 18:14:35 +1100

    Updates

---
+++ precache-manifest.ff9dd43a321873664362762e77bea99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730ef5f700d63df662817fb24269425a",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "8cb277180d76aa8b6306",
+    "url": "/ReactTodoApp/static/css/main.d0a9261d.chunk.css"
+  },
+  {
+    "revision": "38242dfad23ae52babdf",
+    "url": "/ReactTodoApp/static/js/2.c689b07c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactTodoApp/static/js/2.c689b07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb277180d76aa8b6306",
+    "url": "/ReactTodoApp/static/js/main.9da939ee.chunk.js"
+  },
+  {
+    "revision": "e6837cbc16f30b3cb7c3",
+    "url": "/ReactTodoApp/static/js/runtime-main.9ca8ccc2.js"
+  }
+]);
